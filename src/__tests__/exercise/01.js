822b2af92a9d98456348dc6846642acf17a078b9
Fredy Velásquez <macacoloco696@gmail.com>
2024-06-23 15:19:27 -0600

    testing

--- src/__tests__/exercise/01.js
+++ src/__tests__/exercise/01.js
@@ -1,4 +1,5 @@
 // simple test with ReactDOM
+// 💯 use dispatchEvent
 // http://localhost:3000/counter
 
 import * as React from 'react'
@@ -25,11 +26,18 @@ test('counter increments and decrements when the buttons are clicked', () => {
   const message = div.firstChild.querySelector('div')
 
   expect(message.textContent).toBe('Current count: 0')
-  act(() => increment.click())
+  const incrementClickEvent = new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true,
+    button: 0,
+  })
+  act(() => increment.dispatchEvent(incrementClickEvent))
   expect(message.textContent).toBe('Current count: 1')
-  act(() => decrement.click())
+  const decrementClickEvent = new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true,
+    button: 0,
+  })
+  act(() => decrement.dispatchEvent(decrementClickEvent))
   expect(message.textContent).toBe('Current count: 0')
-
-  // extra credit missing
-
 })
