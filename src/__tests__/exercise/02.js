ff823b6c25a8911052933de9e04e263b2d36a89a
Fredy Velásquez <macacoloco696@gmail.com>
2024-06-23 16:22:47 -0600

    testing

--- src/__tests__/exercise/02.js
+++ src/__tests__/exercise/02.js
@@ -1,37 +1,19 @@
 // simple test with React Testing Library
+// 💯 use @testing-library/jest-dom
 // http://localhost:3000/counter
 
 import * as React from 'react'
+import {render, fireEvent} from '@testing-library/react'
 import Counter from '../../components/counter'
-import {render, fireEvent, container} from '@testing-library/react'
-
-global.IS_REACT_ACT_ENVIRONMENT = true
-
 
 test('counter increments and decrements when the buttons are clicked', () => {
-
-
   const {container} = render(<Counter />)
-
   const [decrement, increment] = container.querySelectorAll('button')
   const message = container.firstChild.querySelector('div')
 
-  expect(message.textContent).toBe('Current count: 0')
-
-  // 🐨 replace the next two statements with `fireEvent.click(button)`
-  // 💰 note that you can remove `act` completely!
-  const incrementClickEvent = new MouseEvent('click', {
-    bubbles: true,
-    cancelable: true,
-    button: 0,
-  })
-  act(() => increment.dispatchEvent(incrementClickEvent))
-  expect(message.textContent).toBe('Current count: 1')
-  const decrementClickEvent = new MouseEvent('click', {
-    bubbles: true,
-    cancelable: true,
-    button: 0,
-  })
-  act(() => decrement.dispatchEvent(decrementClickEvent))
-  expect(message.textContent).toBe('Current count: 0')
+  expect(message).toHaveTextContent('Current count: 0')
+  fireEvent.click(increment)
+  expect(message).toHaveTextContent('Current count: 1')
+  fireEvent.click(decrement)
+  expect(message).toHaveTextContent('Current count: 0')
 })
